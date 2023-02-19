7e38dbb710bfa210b730423396797aa040bc16ce
Stijn Rogiest <reddusted@gmail.com>
2023-02-19 18:48:52 +0100

    Use nanoid for pubnub

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import PubNub from "pubnub";
 import { PubNubProvider, usePubNub } from "pubnub-react";
+import { nanoid } from "nanoid";
 
 const pubnub = new PubNub({
     publishKey: process.env.NEXT_PUBLIC_PUB_KEY!,
     subscribeKey: process.env.NEXT_PUBLIC_SUB_KEY!,
-    uuid: process.env.NEXT_PUBLIC_UUID!,
+    uuid: nanoid(),
 });
-
-// if (typeof window !== "undefined") {
-//     pubnub.setUUID(window.crypto.randomUUID());
-// }
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
